09e28ec59e2df421c5f8d2073d5963c254a87d86
KirtuZ <kirtuz@ya.ru>
2022-02-09 14:11:37 +0200

    Preparing auth-users structure.

---
+++ src/components/common/user-data-form.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserDataForm = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default UserDataForm;
--- src/config/services-config.ts
+++ src/config/services-config.ts
@@ -1,6 +1,14 @@
 import ProductServiceFire from "../service/product-service-fire";
 import Catalog from "../service/catalog";
+import AuthServiceFire from "../service/auth-service-fire";
+import UserServiceFire from "../service/user-service-fire";
 
 /*** Product Service Config ***/
 const productService = new ProductServiceFire("products");
 export const catalog: Catalog = new Catalog(productService);
+
+/*** Auth Service Config ***/
+export const authService = new AuthServiceFire();
+
+/*** Users Service Config ***/
+export const userService = new UserServiceFire();
