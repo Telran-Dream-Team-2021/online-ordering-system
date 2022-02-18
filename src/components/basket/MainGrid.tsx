ccd9710223cdefe98a99e5b3609e21628b5c3738
alefmanvladimir <smagazhevskijj@gmail.com>
2022-02-18 07:00:00 +0200

    fix imageUrl in basket

--- src/components/basket/MainGrid.tsx
+++ src/components/basket/MainGrid.tsx
@@ -11,15 +11,15 @@ import {ProductData} from "../../models/product-data";
 
 const MainGrid: FC<{basketData: BasketData, catalogData: ProductData[]}> = (props) => {
     const {basketData, catalogData} = props;
-
+    const dispatch = useDispatch()
     const getRows: (basketData: BasketData) => GridRowsProp = () => {
 
         return basketData.basketItems.map(
             (i) => {
-                const imageUrl = catalogData.find(product => product.productId === i.productId)
+                const product = catalogData.find(product => product.productId === i.productId)
                 return {
                         id: i.productId,
-                        col0: imageUrl ? imageUrl : '',
+                        col0: product ? product.imageUrl : '',
                         col1: catalogData.find(p => p.productId === i.productId)!.name,
                         col2: i,
                         col3: i.pricePerUnit,
@@ -42,7 +42,7 @@ const MainGrid: FC<{basketData: BasketData, catalogData: ProductData[]}> = (prop
         },
         {field: 'col1', headerName: 'Product', width: 150},
         {field: 'col2', headerName: 'Qty', width: 150,
-            renderCell: (params)=><Quantity item={params.value} setItemsStateFn={()=>{basket.updateBasket(basketData.userId, basketData)}}/>},
+            renderCell: (params)=><Quantity item={params.value} setItemsStateFn={()=>{dispatch(updateBasketAction(basketData))}}/>},
         {field: 'col3', headerName: 'Price per unit', width: 150},
         {field: 'col4', headerName: 'Price', width: 150},
     ];
